228487f861f3143bfbca916e6fc0cd5f83f616fa
Gonçalo Mendes <51489398+goncaloRM@users.noreply.github.com>
2025-04-06 18:07:00 -0400

    Add files via upload

---
+++ T11/backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const routes = require("./routes");
+const cors = require("cors");
+
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
+const app = express();
+
+app.use(cors({ origin: FRONTEND_URL }));
+
+app.use(express.json());
+app.use('', routes);
+
+module.exports = app;
